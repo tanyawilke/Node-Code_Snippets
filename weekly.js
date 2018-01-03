1b10aa5899b8769aeeffa3b1acbc23c58bdd9b0e
TanyaWilke <tanya.wilke@gmail.com>
2018-01-03 12:50:29 +0200

    New files
    Added two new code snippets

---
+++ weekly.js
@@ -0,0 +1,9 @@
+var Job = require('./job.js')
+var job = new Job()
+
+job.on('done', function(details){
+  console.log('Weekly email job was completed at',
+    details.completedOn)
+})
+
+job.emit('start')
