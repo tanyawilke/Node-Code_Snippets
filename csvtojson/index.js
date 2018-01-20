047c40b60c047a04ad7af902e6854275898d15f1
TanyaWilke <tanya.wilke@gmail.com>
2018-01-20 13:40:42 +0200

    New files
    Assignment 1

---
+++ csvtojson/index.js
@@ -0,0 +1,22 @@
+// require csvtojson
+var csv = require("csvtojson");
+const fs = require('fs');
+
+let csvFilePath = "customer-data.csv";
+var jsonContent = "";
+// Convert a csv file with csvtojson
+csv()
+.fromFile(csvFilePath)
+.on('json', (jsonObj)=>{
+  jsonContent = JSON.stringify(jsonObj);
+  console.log(jsonContent);
+  fs.appendFile("customer-data.json", jsonContent, 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+ 
+    console.log("JSON file has been saved.");
+  });
+  
+})
